35beaa3cc3a1be68883e38d2dfeb84853b1565b4
전현성 <skyoun97@gmail.com>
2021-02-01 04:18:15 +0900

    Done - Chart

--- src/routes/plan/Plan.component.tsx
+++ src/routes/plan/Plan.component.tsx
@@ -212,7 +212,7 @@ export default function Plan(): JSX.Element {
                 innerRadius={0.5}
                 padAngle={0.7}
                 cornerRadius={3}
-                colors={{ scheme: "nivo" }}
+                colors={{ scheme: "accent" }}
                 borderWidth={1}
                 borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
                 radialLabelsSkipAngle={10}
